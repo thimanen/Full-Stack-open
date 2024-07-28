91e5614782630bf7696649e0e8d88d825d375981
Teemu Himanen <thimanen@gmail.com>
2024-07-28 18:36:30 +0300

    1.6: unicafe step1 final

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,14 +1,53 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  console.log(props.header)
+  return (
+    <h1>
+      {props.header}
+    </h1>
+  )
+}
+
+const Button = (props) => {
+  console.log(props)
+  const { handleClick, text } = props
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <Header header={'give feedback'} />
+      <Button handleClick={handleGoodClick} text='Good' />
+      <Button handleClick={handleNeutralClick} text='Neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <Header header={'statistics'} />
+
+      good {good}<br></br>
+      neutral {neutral}<br></br>
+      bad {bad}
     </div>
   )
 }
