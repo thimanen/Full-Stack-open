a6f75790925de2d07ee608adda37ec9661ede7aa
Teemu Himanen <thimanen@gmail.com>
2024-08-19 17:38:56 +0300

    2.18 maiden_tiedot step1 - final

--- osa2/maiden_tiedot/src/components/Countries.jsx
+++ osa2/maiden_tiedot/src/components/Countries.jsx
@@ -1,35 +1,24 @@
 import Country from './Country'
 
 const Countries = ({countries, nameFilter}) => {
-    if(nameFilter === '') {
-        return (
-            <ul>
-                {countries.map( (country) => {
-                    return (
-                        <li>
-                            {country.name.common}
-                        </li>
-                    )
-                })}  
-            </ul>)
+    if(nameFilter !== '') {
+        const filteredCountries = countries.filter( (country) => country.name.common.toLowerCase().includes(nameFilter))
+        let allData = false
+        if (filteredCountries.length > 10) {
+            return (
+            <div>Too many matches, specify another filter</div>
+            )
         } else {
-            const filteredCountries = countries.filter( (country) => country.name.common.toLowerCase().includes(nameFilter))
-            let allData = false
-            if (filteredCountries.length > 10) {
-                return (
-                    <div>Too many matches, specify another filter</div>
-                )
-            } else {
-                if (filteredCountries.length === 1)
-                    allData = true
-                return (
-                    <ul>
-                        {filteredCountries.map( (country) => (
-                            <Country country={country} allData={allData} />)
-                        )}
-                    </ul>)
-            }
+            if (filteredCountries.length === 1)
+                allData = true
+            return (
+                <ul>
+                    {filteredCountries.map( (country) => (
+                        <Country key={country.name.common} country={country} allData={allData} />)
+                    )}
+                </ul>)
         }
+    }
 }
 
 export default Countries
--- osa2/maiden_tiedot/src/components/Country.jsx
+++ osa2/maiden_tiedot/src/components/Country.jsx
@@ -11,7 +11,14 @@ const Country = ({country, allData}) => {
                 <div>capital {country.capital}</div>
                 <div>area {country.area}</div>
                 <h3>languages:</h3>
-                
+                <ul>
+                    {Object.keys(country.languages).map((key, index) => (
+                        <li key={index}>
+                            {country.languages[key]}                        
+                        </li>
+                    ))}
+                </ul>
+                <h3>flag:</h3>
                 <img src={`${country.flags.png}`} />
 
             </div>
