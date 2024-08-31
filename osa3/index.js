a5ea794dedfa208beb90b9e2215c2e3d5ad9fda1
Teemu Himanen <thimanen@gmail.com>
2024-08-31 18:28:32 +0300

    3.9: puhelinluettelon backend step9 - initial

--- osa3/index.js
+++ osa3/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', getBody = (request, response) => {
   if (request.method === 'POST') 
@@ -98,7 +100,7 @@ let persons = [
     response.json(person)
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
