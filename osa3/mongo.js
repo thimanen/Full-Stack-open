24519a6f59f437c7fca79b990bbc4c34cf1bf6c5
Teemu Himanen <thimanen@gmail.com>
2024-09-01 20:30:09 +0300

    3.12: tietokanta komentoriviltä - final

--- osa3/mongo.js
+++ osa3/mongo.js
@@ -25,7 +25,7 @@ if (process.argv.length === 3) {
     Person.find({}).then((result) => {
         console.log('phonebook:')
         result.forEach((person) => {
-            console.log(person)
+            console.log(`${person.name} ${person.number}`)
         })
         mongoose.connection.close()
     })
