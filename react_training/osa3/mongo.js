7efb17edb61c26fcdc1bfd715049e0bdb3eff817
Teemu Himanen <thimanen@gmail.com>
2024-09-01 19:16:28 +0300

    osa3 - react_training - mongoDB

--- react_training/osa3/mongo.js
+++ react_training/osa3/mongo.js
@@ -21,12 +21,22 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
+/*
 const note = new Note({
-    content: 'HTML is easy',
+    content: 'Mongoose makes thing easy',
     important: true,
 })
 
 note.save().then(result => {
     console.log('note saved')
+    mongoose.connection.close()
+})
+*/
+
+Note.find({}).then(result => {
+    // console.log(result)
+    result.forEach(note => {
+        console.log(note)
+    })
     mongoose.connection.close()
 })
