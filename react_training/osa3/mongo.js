84d0abbbc383236d9f84df2abce3933db88ad3f5
Teemu Himanen <thimanen@gmail.com>
2024-09-10 22:46:14 +0300

    Lint - final

--- react_training/osa3/mongo.js
+++ react_training/osa3/mongo.js
@@ -1,41 +1,41 @@
 const mongoose = require('mongoose')
 
 if(process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = 
+const url =
 `mongodb+srv://thimanen:${password}@fullstack-course.iaaje.mongodb.net/noteApp?
 retryWrites=true&w=majority&appName=FullStack-course`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
-    const noteSchema = new mongoose.Schema({
-      content: String,
-      important: Boolean,
-    })
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+  })
 
-    const Note = mongoose.model('Note', noteSchema)
+  const Note = mongoose.model('Note', noteSchema)
 
 
-    const note = new Note({
-        content: 'Mongoose makes thing easy',
-        important: true,
-    })
+  const note = new Note({
+    content: 'Mongoose makes thing easy',
+    important: true,
+  })
 
-    note.save().then(result => {
-     console.log('note saved')
-    m   ongoose.connection.close()
-    })
+  note.save().then(() => {
+    console.log('note saved')
+    mongoose.connection.close()
+  })
 
-    Note.find({}).then(result => {
+  Note.find({}).then(result => {
     // console.log(result)
-        result.forEach(note => {
-            console.log(note)
-        })
-        mongoose.connection.close()
+    result.forEach(note => {
+      console.log(note)
     })
+    mongoose.connection.close()
+  })
 })
