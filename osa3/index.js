f3810adb8835a791a35d12420d7c7b7e8c20d5f2
Teemu Himanen <thimanen@gmail.com>
2024-09-10 21:46:23 +0300

    3.21. Tietokantaa käyttävä versio Internetiin

--- osa3/index.js
+++ osa3/index.js
@@ -99,6 +99,7 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
+  /* same error for all validation errors */
   if (error.name === 'ValidationError') {
     return response.status(400).json({error: error.message})
   }
