c1c91484389236a48e787177a4b32ff16cdb2556
Teemu Himanen <thimanen@gmail.com>
2024-10-29 19:01:56 +0200

    5.7 blogilistan frontend, step7 - final

--- osa5/src/components/Blog.jsx
+++ osa5/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  
+  const label = visible ? 'hide' : 'view'
+  const showWhenVisible = {display: visible ? '' : 'none'}
+
+  return (
+    <div className="blog">
+      {blog.title} {blog.author}
+      <button onClick={() => setVisible(!visible)}>{label}</button>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>
+          likes: {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
