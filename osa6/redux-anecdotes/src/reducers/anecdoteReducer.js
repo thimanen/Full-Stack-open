9fcc6b8d09c8dca0a406028ba0c0411dbf2a242e
Teemu Himanen <thimanen@gmail.com>
2024-11-20 20:52:47 +0200

    6.15: anekdootit ja backend, step2 - final

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,18 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+/* const getId = () => (100000 * Math.random()).toFixed(0) */
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        id: getId(),
-        votes: 0
-      })
+      state.push(action.payload)
     },
     addVoteToAnecdote(state, action) {
       const id = action.payload
