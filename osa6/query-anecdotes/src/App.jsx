cddf158a810ada07e8360b0478737ce687a9237a
Teemu Himanen <thimanen@gmail.com>
2024-11-28 22:06:45 +0200

    6.20 - final

---
+++ osa6/query-anecdotes/src/App.jsx
@@ -0,0 +1,51 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+ const result = useQuery({
+  queryKey: ['anecdotes'],
+  queryFn: getAnecdotes,
+  retry: 2
+ })
+ console.log(JSON.parse(JSON.stringify(result)))
+
+ if (result.isLoading) {
+  return <div>loading data...</div>
+ }
+
+ if (result.isError) {
+  return <div>anecdote service not available due to problems in server</div>
+ }
+
+ const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
