1d68822305793a34814c6d15c26d0307851eb6f0
Teemu Himanen <thimanen@gmail.com>
2025-01-09 13:12:11 +0200

    8.4: kirjailijan tiedot

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -116,7 +116,7 @@ type Book {
 type Query {
   authorCount: Int!
   bookCount: Int!
-  allBooks: [Book!]!
+  allBooks(author: String): [Book!]!
   allAuthors: [Author!]!
 }
 `
@@ -125,12 +125,17 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
-    allAuthors: () => authors
+    allBooks: (root, args) => {
+      if (args.author)
+        return books.filter((book) => book.author === args.author)
+      else return books
+    },
+    allAuthors: () => authors,
   },
   Author: {
-    bookCount: (root) => books.filter(book => book.author === root.name).length
-  }
+    bookCount: (root) =>
+      books.filter((book) => book.author === root.name).length,
+  },
 }
 
 const server = new ApolloServer({
