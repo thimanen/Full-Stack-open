f46ab4b4f6eac6f3a294ffa546e77d79be938db4
Teemu Himanen <thimanen@gmail.com>
2025-01-09 14:43:47 +0200

    8.7: kirjailijan syntymävuoden päivitys - final

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -128,6 +128,11 @@ type Mutation {
     published: Int!
     genres: [String!]!  
   ): Book
+
+  editAuthor(
+    name: String!
+    setBornTo: Int!
+  ): Author
 }
 `
 
@@ -165,6 +170,14 @@ const resolvers = {
       }
 
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+      if (!author) return null
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a))
+      return updatedAuthor
     },
   },
 }
